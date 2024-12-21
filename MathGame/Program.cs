27c72ff3e38979d7ed29187a56836b79f5a82b7a
Mans99 <mans.englund@live.se>
2024-12-21 18:27:28 +0100

    initialized .net project

---
+++ MathGame/Program.cs
@@ -0,0 +1,19 @@
+﻿
+class Program() {
+    char c;
+    static void Main(string[] args) {
+        Program p = new Program();
+        while(true) {
+            p.printMenu();
+            break;
+        }
+    }
+
+
+
+
+    void printMenu() {
+        Console.Write("banana");
+    }
+
+}
